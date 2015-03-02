8d40eb3017f0ea73300f9f0e637ad0bb40fd152a
Joey Hagner <jwhagner@gmail.com>
2015-03-02 13:11:48 -0500

    Added my tests to the project

--- src/NoSetup.java
+++ src/NoSetup.java
@@ -1,3 +1,5 @@
+package tests;
+
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
@@ -6,25 +8,58 @@ import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.imageio.ImageIO;
 
-import org.openqa.selenium.*;
 import org.junit.*;
 
 import static org.junit.Assert.*;
 
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-//Test Image
-//http://i.imgur.com/xY3TaEV.png
+public class NoSetup {
+  private WebDriver driver;
+  private String baseUrl;
+  private StringBuffer verificationErrors = new StringBuffer();
 
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://imgur.com";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
 
-public class webtests { 
-	WebDriver driver = new FirefoxDriver();
-	
+  @Test
+  public void testSearch() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.cssSelector("div.search-icon-container-icon")).click();
+    driver.findElement(By.name("q")).clear();
+    driver.findElement(By.name("q")).sendKeys("funny");
+    driver.findElement(By.xpath("//form[@class=\"search-form\"]")).submit();
+    driver.findElement(By.cssSelector("a.image-list-link > img")).click();
+    driver.findElement(By.cssSelector("#tags-underbar > span")).click();
+    List<WebElement> list = driver.findElements(By.xpath("//*[contains(text(),'" + "funny" + "')]"));
+    // Warning: verifyTextPresent may require manual changes
+    try {
+      assertTrue(list.size() > 0);
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    // ERROR: Caught exception [unknown command []]
+  }
+  
+  @Test
+  public void testLogin() throws Exception {
+	  login();
+	  String accountName = driver.findElement(By.xpath("//a[@class='account-user-name']")).getText();
+	  assertTrue(accountName.equals("cs1699testing"));
+  }
+  
 	@Test
 	public void HomePagePictures() throws Exception {
 		driver.get("http://imgur.com/");
@@ -32,12 +67,11 @@ public class webtests {
 		List<WebElement> posts = driver.findElements(By.className("post"));
 		List<String> ids = new ArrayList<String>();
 		for ( WebElement post: posts) {
-			System.out.println(post.getAttribute("id"));
 			if(ids.contains(post.getAttribute("id"))){
-			dups = false;
+				dups = false;
 			}
 			else{
-			ids.add(post.getAttribute("id"));
+				ids.add(post.getAttribute("id"));
 			}		
 		}
 		Assert.assertTrue(dups);
@@ -53,7 +87,6 @@ public class webtests {
 		test.click();
 		test = driver.findElement(By.id("outside-tagging"));
 		String opacity = test.getAttribute("style");
-		System.out.println(opacity);
 		Assert.assertEquals(opacity,"opacity: 1;");
 		driver.quit();
 	  }
@@ -66,17 +99,15 @@ public class webtests {
 	    clipboard.setContents(selection, selection);
 		driver.get("http://imgur.com/");
 		driver.findElement(By.className("upload-button")).click();
-		driver.findElement(By.id("upload-global-link-input")).sendKeys(Keys.COMMAND + "v");
+		WebElement textArea = driver.findElement(By.id("upload-global-link-input"));
+		new Actions( driver ).contextClick(textArea).sendKeys( "p" ).perform();
 		driver.findElement(By.id("upload-global-start-button")).submit();
 		WebElement nicetime = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.id("nicetime")));
 		driver.getCurrentUrl();
 		String code = driver.getCurrentUrl().substring(17);
-		System.out.println(code);
 		driver.get("http://i.imgur.com/"+code+".png");
 		String s = driver.findElement(By.className("shrinkToFit")).getAttribute("src");
-		System.out.println(s);
 		URL url = new URL(s);
-	    System.out.println(url);
 	    BufferedImage link = ImageIO.read(url);
 	    BufferedImage base = ImageIO.read(new File("base.png"));
 	    Assert.assertTrue(imagesAreEqual(link,base));
@@ -91,7 +122,8 @@ public class webtests {
 	    clipboard.setContents(selection, selection);
 		driver.get("http://imgur.com/");
 		driver.findElement(By.className("upload-button")).click();
-		driver.findElement(By.id("upload-global-link-input")).sendKeys(Keys.COMMAND + "v");
+		WebElement textArea = driver.findElement(By.id("upload-global-link-input"));
+		new Actions( driver ).contextClick(textArea).sendKeys( "p" ).perform();
 		driver.findElement(By.id("upload-global-start-button")).submit();
 		WebElement nicetime = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.id("nicetime")));
 		String time1 = nicetime.getText();
@@ -100,9 +132,7 @@ public class webtests {
 		driver.get(pageurl);
 		nicetime = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.id("nicetime")));
 		String time2 = nicetime.getText();
-		System.out.println(time1);
-		System.out.println(time2);
-	    Assert.assertFalse(time1.equals(time2));
+		   Assert.assertFalse(time1.equals(time2));
 	    driver.quit();
 	  }
 	  
@@ -113,11 +143,9 @@ public class webtests {
 		WebElement test;
 		test = driver.findElement(By.className("post"));
 		String PostName = test.getAttribute("id");
-		System.out.println(PostName);
 		test = test.findElement(By.className("image-list-link"));
 		test.click();
 		String currurl = driver.getCurrentUrl();
-		System.out.println(currurl);
 		Assert.assertEquals(currurl,"http://imgur.com/gallery/"+PostName);
 		driver.quit();
 	  }
@@ -150,9 +178,7 @@ public class webtests {
 	public void ImageCheck() throws Exception {
 		driver.get("http://i.imgur.com/FkVYvno.png");
 		String s = driver.findElement(By.className("shrinkToFit")).getAttribute("src");
-		System.out.println(s);
 		URL url = new URL(s);
-	    System.out.println(url);
 	    BufferedImage link = ImageIO.read(url);
 	    BufferedImage base = ImageIO.read(new File("base.png"));
 	    Assert.assertTrue(imagesAreEqual(link,base));
@@ -175,20 +201,39 @@ public class webtests {
 		WebElement test;
 		test = driver.findElement(By.id("content"));
 		Dimension orignal = test.getSize();
-		System.out.println(orignal);
 		Thread.sleep(3000);
 		JavascriptExecutor jsx = (JavascriptExecutor)driver;
 		jsx.executeScript("window.scrollBy(0,2566)", "");
 		Thread.sleep(3000);
 		test = driver.findElement(By.id("content"));
 		Dimension scroll = test.getSize();
-		System.out.println(scroll);
 		Assert.assertTrue((orignal.getHeight()!= scroll.getHeight())&&(orignal.getWidth()==scroll.getWidth()));
 		driver.quit();
 	  }
-	
-	
-	
+  
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+  
+  private void login() {
+	  driver.get(baseUrl + "/signin");
+	  driver.findElement(By.id("username")).sendKeys("cs1699testing");
+	  driver.findElement(By.id("password")).sendKeys("laboontesting");
+	  driver.findElement(By.name("submit")).click();
+  }
+  
+  public void logout() {
+	  WebElement hoverElement = driver.findElement(By.id("secondary-nav"));
+	  Actions builder = new Actions(driver);
+	  builder.moveToElement(hoverElement).perform();
+	  driver.findElement(By.xpath("//div[@class='dropdown-footer']//a[contains(text(),'logout')]")).click();
+  }
+  
 //Helper function to compare image buffers    
 public boolean imagesAreEqual(BufferedImage image1, BufferedImage image2) {
 	if (image1.getWidth() != image2.getWidth() || image1.getHeight() != image2.getHeight()) {
@@ -201,10 +246,6 @@ public boolean imagesAreEqual(BufferedImage image1, BufferedImage image2) {
 	        }
 	    }
 	}
-	return true;
+		return true;
 	} 
-    
-
 }
-
-
